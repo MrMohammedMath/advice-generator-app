62001a15ef5a2e7b831515bb6760a5cb45e25fc0
Mohammed Hassan <mr.math2024@gmail.com>
2023-03-07 18:55:50 +0200

    adding css modules

--- src/components/Advice.tsx
+++ src/components/Advice.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import style from "../scss/advice.module.scss";
 
 export default function Advice() {
     const [advice, setAdvice] = React.useState<{ text: string, id: number } | undefined>(undefined);
@@ -18,17 +19,17 @@ export default function Advice() {
     }
 
     return (
-        <div className="advice">
-            <span className="advice__id">Advice #{advice?.id}</span>
-            <h1 className="advice__text">&#8220;{advice?.text}&#8221;</h1>
+        <div className={style.advice}>
+            <span className={style.advice__id}>Advice #{advice?.id}</span>
+            <h1 className={style.advice__text}>&#8220;{advice?.text}&#8221;</h1>
 
             <picture>
                 <source srcSet="./images/pattern-divider-desktop.svg" media="(min-width: 768px)" />
-                <img src="./images/pattern-divider-mobile.svg" alt="divider" className="advice__divider" />
+                <img src="./images/pattern-divider-mobile.svg" alt="divider" className={style.advice__divider} />
             </picture>
 
-            <button className="advice__btn" onClick={handleClick}>
-                <img src="./images/icon-dice.svg" alt="dice icon" className="advice__btn__icon" />
+            <button className={style.advice__btn} onClick={handleClick}>
+                <img src="./images/icon-dice.svg" alt="dice icon" className={style.advice__btn__icon} />
             </button>
         </div>
     );
